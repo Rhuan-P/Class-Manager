f12eca95ed0d262a6eb68a50b690b63797448ae0
Rhuan Pablo Monteiro Da Silva <rhuanpablomonteiro@gmail.com>
2024-06-22 21:13:22 -0300

    Update ClassManager.py

--- Project/Code/ClassManager.py
+++ Project/Code/ClassManager.py
@@ -1,3 +1,4 @@
+# Definição da Superclasse Usuario
 class Usuario:
     def __init__(self, nome, idade, endereco, telefone, email, data_nascimento, genero, cpf, matricula):
         self.nome = nome
@@ -11,7 +12,7 @@ class Usuario:
         self.matricula = matricula
 
     def login(self, matricula, cpf):
-        pass 
+        pass
         # TODO
 
     def info(self):
@@ -20,7 +21,7 @@ class Usuario:
                 f'Data de Nascimento: {self.data_nascimento}, Gênero: {self.genero}, '
                 f'CPF: {self.cpf}, Matrícula: {self.matricula}')
 
-
+# Definição da Subclasse Aluno 
 class Aluno(Usuario):
     def __init__(self, nome, idade, endereco, telefone, email, data_nascimento, genero, cpf, matricula, turma):
         super().__init__(nome, idade, endereco, telefone, email, data_nascimento, genero, cpf, matricula)
@@ -29,7 +30,7 @@ class Aluno(Usuario):
     def info(self):
         return f'{super().info()}, Turma: {self.turma}'
 
-
+# Definição da Subclasse 
 class Professor(Usuario):
     def __init__(self, nome, idade, endereco, telefone, email, data_nascimento, genero, cpf, matricula, salario):
         super().__init__(nome, idade, endereco, telefone, email, data_nascimento, genero, cpf, matricula)
