3bb08e546ce7ba2c037175386c5bb6fa37d64225
Rhuan Pablo Monteiro Da Silva <rhuanpablomonteiro@gmail.com>
2024-06-22 21:10:30 -0300

    "Classes Usuario, Aluno e Professor, com métodos detalhados e herança"

---
+++ Project/Code/ClassManager.py
@@ -0,0 +1,39 @@
+class Usuario:
+    def __init__(self, nome, idade, endereco, telefone, email, data_nascimento, genero, cpf, matricula):
+        self.nome = nome
+        self.idade = idade
+        self.endereco = endereco
+        self.telefone = telefone
+        self.email = email
+        self.data_nascimento = data_nascimento
+        self.genero = genero
+        self.cpf = cpf
+        self.matricula = matricula
+
+    def login(self, matricula, cpf):
+        pass 
+        # TODO
+
+    def info(self):
+        return (f'Nome: {self.nome}, Idade: {self.idade}, Endereço: {self.endereco}, '
+                f'Telefone: {self.telefone}, Email: {self.email}, '
+                f'Data de Nascimento: {self.data_nascimento}, Gênero: {self.genero}, '
+                f'CPF: {self.cpf}, Matrícula: {self.matricula}')
+
+
+class Aluno(Usuario):
+    def __init__(self, nome, idade, endereco, telefone, email, data_nascimento, genero, cpf, matricula, turma):
+        super().__init__(nome, idade, endereco, telefone, email, data_nascimento, genero, cpf, matricula)
+        self.turma = turma
+
+    def info(self):
+        return f'{super().info()}, Turma: {self.turma}'
+
+
+class Professor(Usuario):
+    def __init__(self, nome, idade, endereco, telefone, email, data_nascimento, genero, cpf, matricula, salario):
+        super().__init__(nome, idade, endereco, telefone, email, data_nascimento, genero, cpf, matricula)
+        self.salario = salario
+
+    def info(self):
+        return f'{super().info()}, Salario: R$ {self.salario:.2f}'
